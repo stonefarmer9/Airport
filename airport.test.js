9b71ccabb8571e9eac50f2ff22e65537be0ce100
stonefarmer9 <g.p.levaillant@gmail.com>
2019-01-16 12:54:27 +0000

    1st test updated to return plane object

--- airport.test.js
+++ airport.test.js
@@ -2,5 +2,5 @@ const Airport = require('./airport')
 airport = new Airport('Bristol')
 console.log(airport)
 test('the airport lands a plane', () => {
-  expect(airport.land()).toBe("plane");
+  expect(airport.land(plane)).toBe(plane);
 })
