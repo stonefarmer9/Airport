3569833832f137ae82109b7cd0fa0fd30280e6c2
stonefarmer9 <g.p.levaillant@gmail.com>
2019-01-18 13:03:16 +0000

    Unit test for weather updated to use correct matcher and simple pass implemenet

---
+++ weather.js
@@ -0,0 +1,13 @@
+class Weather {
+
+  getWeather() {
+    var chance = Math.random().floor * 10
+    if (chance <= 3) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+module.exports = Weather;
